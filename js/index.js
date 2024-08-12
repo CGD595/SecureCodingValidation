0a74061a12811a68b2838586c51abb0f8d7c8b17
Chime Gyeltshen Dorji <chimegyeltshendorji2004@gmail.com>
2024-08-12 12:03:49 +0600

    v

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const confirmPasswordInput = document.getElementById("confirm-password");
 const passToggleBtn = document.getElementById("pass-toggle-btn");
 const confirmPassToggleBtn = document.getElementById("confirm-pass-toggle-btn");
 
-// Function to display error messages
+
 const showError = (field, errorText) => {
     field.classList.add("error");
     const errorElement = document.createElement("small");
@@ -13,40 +13,34 @@ const showError = (field, errorText) => {
     field.closest(".form-group").appendChild(errorElement);
 }
 
-// Function to handle form submission
 const handleFormData = (e) => {
     e.preventDefault();
 
-    // Retrieving input elements
     const fullnameInput = document.getElementById("fullname");
     const emailInput = document.getElementById("email");
-    const dateInput = document.getElementById("date");
     const genderInput = document.getElementById("gender");
     const ageInput = document.getElementById("age");
     const cidInput = document.getElementById("cid");
 
-    // Getting trimmed values from input fields
     const fullname = fullnameInput.value.trim();
     const email = emailInput.value.trim();
     const password = passwordInput.value.trim();
     const confirmPassword = confirmPasswordInput.value.trim();
-    const date = dateInput.value;
     const gender = genderInput.value;
     const age = ageInput.value.trim();
     const cid = cidInput.value.trim();
 
-    // Regular expression patterns for validation
     const namePattern = /^[a-zA-Z\s]+$/;
     const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     const agePattern = /^[0-9]{1,3}$/;
     const cidPattern = /^\d{11}$/;
 
-    // Clearing previous error messages
+
     document.querySelectorAll(".form-group .error").forEach(field => field.classList.remove("error"));
     document.querySelectorAll(".error-text").forEach(errorText => errorText.remove());
 
-    // Performing validation checks
+
     if (fullname === "") {
         showError(fullnameInput, "Enter your Username");
     } else if (!namePattern.test(fullname)) {
@@ -81,23 +75,17 @@ const handleFormData = (e) => {
         showError(cidInput, "Citizen ID must be an 11-digit number and not alphabets or special characters");
     } 
 
-    if (date === "") {
-        showError(dateInput, "Select your date of birth");
-    }
-
     if (gender === "") {
         showError(genderInput, "Select your gender");
     }
     
-    // Checking for any remaining errors before form submission
+
     const errorInputs = document.querySelectorAll(".form-group .error");
     if (errorInputs.length > 0) return;
 
-    // Submitting the form
     form.submit();
 }
 
-// Toggling password visibility
 passToggleBtn.addEventListener('click', () => {
     passToggleBtn.className = passwordInput.type === "password" ? "fa-solid fa-eye-slash" : "fa-solid fa-eye";
     passwordInput.type = passwordInput.type === "password" ? "text" : "password";
@@ -108,5 +96,4 @@ confirmPassToggleBtn.addEventListener('click', () => {
     confirmPasswordInput.type = confirmPasswordInput.type === "password" ? "text" : "password";
 });
 
-// Handling form submission event
 form.addEventListener("submit", handleFormData);
